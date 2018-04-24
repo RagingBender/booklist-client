dbe6f39f91c0ce486ff570363cc5419125ec7bb3
jackie <jcqnly@gmail.com>
2018-04-23 22:03:19 -0700

    add url

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,7 +5,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://jl-tc-pk-booklist.herokuapp.com/';
+ENV.productionApiUrl = 'https://ragingbender.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
