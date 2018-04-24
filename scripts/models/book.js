158c53aafd52a9dd47f29b6b8574330edda35d4c
jackie <jcqnly@gmail.com>
2018-04-23 22:20:18 -0700

    add console.logs

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -25,7 +25,10 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
   };
 
   Book.all = [];
-  Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  Book.loadAll = rows => {
+    console.log('this is a row',rows);
+    Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  };
 
   Book.fetchAll = callback =>
     $.get(`${ENV.apiUrl}/api/v1/books`)
